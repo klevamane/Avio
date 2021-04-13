1cb6680c24c9d2b694c20bf552c205f8b7de38c8
Onengiye Onengiye <klevamane@gmail.com>
2021-04-13 13:43:47 +0100

    add user login action and user login reducer

---
+++ src/reducers/user.reducers.js
@@ -0,0 +1,24 @@
+import {
+  USER_LOGIN_FAIL,
+  USER_LOGIN_REQUEST,
+  USER_LOGIN_SUCCESS,
+  USER_LOGOUT,
+} from '../constants/user.constants';
+
+export const userLoginReducer = (state = {}, action) => {
+  switch (action.type) {
+    case USER_LOGIN_REQUEST:
+      return { ...state, loading: true, products: [] };
+    case USER_LOGIN_SUCCESS:
+      return { ...state, loading: false, userInfo: action.payload };
+
+    case USER_LOGIN_FAIL:
+      return { ...state, loading: false, error: action.payload };
+
+    case USER_LOGOUT:
+      return {};
+
+    default:
+      return state;
+  }
+};
--- src/store.js
+++ src/store.js
@@ -4,19 +4,29 @@ import { productDetailsReducer, productListReducer } from './reducers/product';
 import { cartReducer } from './reducers/cart.reducers';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunkMiddleware from 'redux-thunk';
+import { userLoginReducer } from './reducers/user.reducers';
 
 console.log('WELCOME TO THE STORE');
 const rootReducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
   cart: cartReducer,
+  userLogin: userLoginReducer,
 });
 const middleware = [thunkMiddleware];
 
 const cartItemsFromLocalStorage = localStorage.getItem('cartItems')
   ? JSON.parse(localStorage.getItem('cartItems'))
   : [];
-const initialState = { cart: { cartItems: cartItemsFromLocalStorage } };
+
+const userInfoFromStorage = localStorage.getItem('userInfo')
+  ? JSON.parse(localStorage.getItem('userInfo'))
+  : null;
+
+const initialState = {
+  cart: { cartItems: cartItemsFromLocalStorage },
+  userInfo: userInfoFromStorage,
+};
 const store = createStore(
   rootReducer,
   initialState,
