793e0896d71255b690974240b00480cbe5a0010d
Onengiye Onengiye <klevamane@gmail.com>
2021-05-10 13:32:43 +0100

    remove environment variable

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import { Route, BrowserRouter as Router } from 'react-router-dom';
 import {
-	faShoppingCart,
-	faStar,
-	faStarHalf,
-	faStarHalfAlt,
-	faTrash,
-	faUser,
+  faShoppingCart,
+  faStar,
+  faStarHalf,
+  faStarHalfAlt,
+  faTrash,
+  faUser,
 } from '@fortawesome/free-solid-svg-icons';
 
 import CartScreen from './screens/cart.screen';
 import { Container } from 'react-bootstrap';
 import Footer from './components/footer.component';
-import Header from './components/header.component';
+import Header from './components/header';
 import HomeScreen from './screens/home.screen';
 import OrderListScreen from './screens/orderList.screens';
+import OrderScreen from './screens/order.screen';
 import PaymentScreen from './screens/payment.screen';
 import PlaceOrderScreen from './screens/placeOrder.screen';
 import ProductEditScreen from './screens/productEdit.screen';
@@ -28,72 +29,70 @@ import { fab } from '@fortawesome/free-brands-svg-icons';
 import { far } from '@fortawesome/free-regular-svg-icons';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import loginScreen from './screens/login.screen';
-import logo from './logo.svg';
-import OrderScreen from './screens/order.screen';
 
 const App = () => {
-	return (
-		<Router>
-			<Header />
-			<main className='py-3'>
-				<Container>
-					<Route path={'/'} component={HomeScreen} exact />
-					<Route path={'/auth/login'} component={loginScreen} exact />
-					<Route path={'/auth/signup'} component={SignupScreen} exact />
-					<Route path={'/product/:id'} component={ProductScreen} exact />
-					<Route path={'/user/profile'} component={ProfileScreen} exact />
-					<Route path={'/order/:id'} component={OrderScreen} exact />
+  return (
+    <Router>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <Route path={'/'} component={HomeScreen} exact />
+          <Route path={'/auth/login'} component={loginScreen} exact />
+          <Route path={'/auth/signup'} component={SignupScreen} exact />
+          <Route path={'/product/:id'} component={ProductScreen} exact />
+          <Route path={'/user/profile'} component={ProfileScreen} exact />
+          <Route path={'/order/:id'} component={OrderScreen} exact />
 
-					{/* Admin Routes */}
-					<Route path={'/admin/users'} component={UsersScreen} />
-					<Route path={'/admin/user/:id'} component={UserGetEditScreen} />
-					<Route
-						path={'/admin/products'}
-						component={ProductsListScreen}
-						exact
-					/>
-					{/* Pagination */}
-					<Route
-						path={'/admin/products/:pageNumber'}
-						component={ProductsListScreen}
-						exact
-					/>
-					<Route path={'/admin/orders'} component={OrderListScreen} exact />
-					<Route
-						path={'/admin/products/edit/:id'}
-						component={ProductEditScreen}
-					/>
-					<Route path={'/search/:keyword'} component={HomeScreen} exact />
-					<Route path={'/page/:pageNumber'} component={HomeScreen} exact />
+          {/* Admin Routes */}
+          <Route path={'/admin/users'} component={UsersScreen} />
+          <Route path={'/admin/user/:id'} component={UserGetEditScreen} />
+          <Route
+            path={'/admin/products'}
+            component={ProductsListScreen}
+            exact
+          />
+          {/* Pagination */}
+          <Route
+            path={'/admin/products/:pageNumber'}
+            component={ProductsListScreen}
+            exact
+          />
+          <Route path={'/admin/orders'} component={OrderListScreen} exact />
+          <Route
+            path={'/admin/products/edit/:id'}
+            component={ProductEditScreen}
+          />
+          <Route path={'/search/:keyword'} component={HomeScreen} exact />
+          <Route path={'/page/:pageNumber'} component={HomeScreen} exact />
 
-					{/* if we have a search result with pagination then */}
-					{/* Pagination */}
-					<Route
-						path={'/search/:keyword/page/:pageNumber'}
-						component={HomeScreen}
-						exact
-					/>
+          {/* if we have a search result with pagination then */}
+          {/* Pagination */}
+          <Route
+            path={'/search/:keyword/page/:pageNumber'}
+            component={HomeScreen}
+            exact
+          />
 
-					{/* make the id optional */}
-					<Route path={'/cart/:id?'} component={CartScreen} exact />
-					<Route path={'/shipping'} component={ShippingScreen} exact />
-					<Route path={'/payment'} component={PaymentScreen} exact />
-					<Route path={'/placeorder'} component={PlaceOrderScreen} exact />
-				</Container>
-			</main>
-			<Footer />
-		</Router>
-	);
+          {/* make the id optional */}
+          <Route path={'/cart/:id?'} component={CartScreen} exact />
+          <Route path={'/shipping'} component={ShippingScreen} exact />
+          <Route path={'/payment'} component={PaymentScreen} exact />
+          <Route path={'/placeorder'} component={PlaceOrderScreen} exact />
+        </Container>
+      </main>
+      <Footer />
+    </Router>
+  );
 };
 
 library.add(
-	fab,
-	far,
-	faUser,
-	faShoppingCart,
-	faStarHalf,
-	faStar,
-	faStarHalfAlt,
-	faTrash
+  fab,
+  far,
+  faUser,
+  faShoppingCart,
+  faStarHalf,
+  faStar,
+  faStarHalfAlt,
+  faTrash,
 );
 export default App;
---
+++ src/components/header.js
@@ -0,0 +1,93 @@
+import { Container, Nav, NavDropdown, Navbar } from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { LinkContainer } from 'react-router-bootstrap';
+import React from 'react';
+import { Route } from 'react-router';
+import SearchBox from './searchbox.component';
+import { logout } from '../actions/auth.actions';
+
+const Header = () => {
+  const dispatch = useDispatch();
+  const authLoginInfo = useSelector((state) => state.authLoginInfo);
+
+  const { loggedInUserInfo } = authLoginInfo;
+
+  const handleLogout = () => {
+    dispatch(logout());
+  };
+
+  return (
+    <header>
+      <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
+        <Container>
+          <LinkContainer to='/'>
+            <Navbar.Brand>AVIO</Navbar.Brand>
+          </LinkContainer>
+          {/* we need to pass history to this component as the component cannot
+					on it's own access the history props, since it is embeded in another component
+					ie the Header component
+					One other we this can be done is to access the history props from the header
+					component, and pass the history props to the search component */}
+          <Route render={({ history }) => <SearchBox history={history} />} />
+          <Navbar.Toggle aria-controls='basic-navbar-nav' />
+          <Navbar.Collapse id='basic-navbar-nav'>
+            <Nav className='ml-auto'>
+              <LinkContainer to='/cart'>
+                <Nav.Link>
+                  Cart <FontAwesomeIcon icon='shopping-cart' />
+                </Nav.Link>
+              </LinkContainer>
+              {loggedInUserInfo && loggedInUserInfo.user ? (
+                <NavDropdown
+                  title={loggedInUserInfo.user.name}
+                  id='basic-nav-dropdown'
+                >
+                  <LinkContainer to={'/user/profile'}>
+                    <NavDropdown.Item>Profile</NavDropdown.Item>
+                  </LinkContainer>
+                  {/* <NavDropdown.Item>
+                    <Link to={'/user/profile'}>Profile</Link>
+                  </NavDropdown.Item> */}
+
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item onClick={handleLogout}>
+                    Logout
+                  </NavDropdown.Item>
+                </NavDropdown>
+              ) : (
+                <LinkContainer to='/auth/login'>
+                  <Nav.Link>
+                    SignIn <FontAwesomeIcon icon='user' />
+                  </Nav.Link>
+                </LinkContainer>
+              )}
+              {loggedInUserInfo &&
+                loggedInUserInfo.user &&
+                loggedInUserInfo.user.isAdmin && (
+                  <NavDropdown title='Admin' id='admin-menu'>
+                    <LinkContainer to={'/admin/users'}>
+                      <NavDropdown.Item>Users</NavDropdown.Item>
+                    </LinkContainer>
+
+                    <NavDropdown.Divider />
+                    <LinkContainer to={'/admin/products'}>
+                      <NavDropdown.Item>Products</NavDropdown.Item>
+                    </LinkContainer>
+
+                    <NavDropdown.Divider />
+                    <LinkContainer to={'/admin/orders'}>
+                      <NavDropdown.Item>Orders</NavDropdown.Item>
+                    </LinkContainer>
+                  </NavDropdown>
+                )}
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
+};
+
+export default Header;
